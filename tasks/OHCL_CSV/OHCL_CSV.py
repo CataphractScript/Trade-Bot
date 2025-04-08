dba868bcfd212098823bf20322287169c802b3e7
CataphractScript <dowlatabadiparsa@gmail.com>
2025-04-08 23:28:49 +0330

    test mt5 functions

--- tasks/OHCL_CSV/OHCL_CSV.py
+++ tasks/OHCL_CSV/OHCL_CSV.py
@@ -1,7 +1,13 @@
 import MetaTrader5 as mt5
-# from datetime import datetime
-# import csv
+from datetime import datetime
+import csv
 
 if not mt5.initialize():
     print("Initialization failed:", mt5.last_error())
     quit()
+
+symbol = "EURUSD"
+
+mt5.symbol_select(symbol, True)
+
+rates = mt5.copy_rates_from_pos(symbol, mt5.TIMEFRAME_M15, 0, 20)
