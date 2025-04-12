29974e039419c7614c63aafea9ceef9e558d4777
CataphractScript <dowlatabadiparsa@gmail.com>
2025-04-13 00:16:35 +0330

    Save OHCL to data.csv

--- tasks/OHCL_CSV/OHCL_CSV.py
+++ tasks/OHCL_CSV/OHCL_CSV.py
@@ -1,6 +1,6 @@
 import MetaTrader5 as mt5
 from datetime import datetime
-import csv
+import pandas as pd
 
 if not mt5.initialize():
     print("Initialization failed:", mt5.last_error())
@@ -11,3 +11,7 @@ symbol = "EURUSD"
 mt5.symbol_select(symbol, True)
 
 rates = mt5.copy_rates_from_pos(symbol, mt5.TIMEFRAME_M15, 0, 20)
+
+ohcl = pd.DataFrame(rates)
+
+ohcl.to_csv('data.csv')
