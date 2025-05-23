1a15d1c497606e12e4fda3387f487125acb9bc05
CataphractScript <dowlatabadiparsa@gmail.com>
2025-05-23 12:05:51 +0330

    Form1.cs Update

--- bot/MVP/Visualize/Form1.cs
+++ bot/MVP/Visualize/Form1.cs
@@ -1,9 +1,13 @@
+using LiveChartsCore;
+using LiveChartsCore.SkiaSharpView.WinForms;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace Visualize
 {
     public partial class Form1 : Form
     {
+
         public Form1()
         {
             InitializeComponent();
@@ -11,8 +15,11 @@ namespace Visualize
 
         private async void Form1_Load(object sender, EventArgs e)
         {
-            Logger.Initialize(logTextBox);
-            await LBankWebSocket.ListenToLBank();
+            cartesianChart1.ZoomMode = LiveChartsCore.Measure.ZoomAndPanMode.Both;
+
+            ChartHelper.InitializeChart(cartesianChart1);
+            Logger.Initialize(log_text_box);
+            await LBankWebSocket.ListenToLBank(cartesianChart1);
         }
 
         private void cartesianChart1_Load(object sender, EventArgs e)
@@ -28,6 +35,11 @@ namespace Visualize
         private void richTextBox1_TextChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void reload_button_Click(object sender, EventArgs e)
+        {
+            ChartHelper.InitializeChart(cartesianChart1);
         }
     }
 }
